4a1290bfe485d6b50f536e8fafc6c84d6a5836bf
Vineetha Sreerenj <vineethaav@gmail.com>
2019-06-30 20:09:37 -0700

    Program to find area of circle with given radius
    This program finds area of the circle with the given radius. Shows the
    import and use of math module

---
+++ circlearea.py
@@ -0,0 +1,9 @@
+'''Write a Python program which accepts the radius of a circle from the user and compute the area. Go to the editor
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504'''
+
+
+from math import pi
+r = float(input("Enter radius:"))
+print("Area =",pi*(r*r))
